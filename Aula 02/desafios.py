4ca944902aef2419f3663e37cbea1adab28a9e90
PROGRAMAÇÃO EM PYTHON 2025.1 <51129532025.1@senacrjedu.local>
2025-04-30 20:16:35 -0300

    Aula 02

---
+++ Aula 02/desafios.py
@@ -0,0 +1,12 @@
+"""
+
+1 – Construa um algoritmo em Python que leia 20 valores inteiros
+e calcule seu somatório.
+
+"""
+
+"""
+2 – Construa um programa em python que solicite ao usuário um  
+valor entre 10 e 100,  caso o usuário digite  um valor menor que 10 ou maior que 100, envie uma mensagem informando que os valores são inválidos caso contrário conte de zero até o numero digitado pelo usuário
+
+"""
---
+++ Aula 02/repeticao.py
@@ -0,0 +1,20 @@
+#Estrutura de repeticao
+
+#Enquanto 
+limite = 0
+
+while limite < 10:
+    print(f"Viagem: {limite}")
+    limite = limite + 1
+
+#Repeticao com for
+#Forma simples
+for contado in range(20):
+    print(f"Viagen vom for: {contado}")
+
+#Ler uma lista
+caixa_frutas = ["Maçã","Pera","Melancia"]
+for fruta in caixa_frutas:
+    print(f"A fruta é: {fruta}")
+
+
