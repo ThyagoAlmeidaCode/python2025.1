85170935aeee1d05edac44fb5da906b7d7db491d
PROGRAMAÇÃO EM PYTHON 2025.1 <51129532025.1@senacrjedu.local>
2025-05-07 18:47:47 -0300

    Aula 02

--- Aula 02/game.py
+++ Aula 02/game.py
@@ -10,12 +10,13 @@ secreto_num = 12
 
 tentativas = int(input("Quantas vezes deseja jogar? "))
 
-for tentativa in range(1,tentativas):
+for tentativa in range(1,tentativas+1): #Adiconar mais 1 ao range
     print(f"Tentativa {tentativa} de {tentativas}")
-    palpite = input("Informe seu palpite: ")
+    palpite =  int( input("Informe seu palpite: ") ) #Converter para Inteiro
     print("Seu palpite é: {}".format(palpite))
     if (secreto_num == palpite):
         print("Você acertou miseravi")
+        break #Encerra o jogo (Encerra o if a repetição)
     else:
         print("Você errou miseravi")
 
