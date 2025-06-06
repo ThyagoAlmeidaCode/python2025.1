9747a1c83f431ce57a6ddb478e71e02d1833fd1a
PROGRAMAÇÃO EM PYTHON 2025.1 <51129532025.1@senacrjedu.local>
2025-06-06 19:04:59 -0300

    Aula 16

--- Projetos/banco/clientes/views.py
+++ Projetos/banco/clientes/views.py
@@ -17,11 +17,19 @@ class Listagem(ListView):
     model = Cliente #Conecta ao modelo de banco de dados, E Retorna uma lista chamamda cliente_list    
     template_name = 'clientes/list_cliente.html'#Conecta ao arquivo html do templates
 
-#Classee de cadastro
+#Clase de cadastro
 class Cadastro(CreateView):
     model = Cliente
     form_class = ClienteForm
     template_name = 'clientes/forms_cliente.html'   #Gera o template do formulario
+    
+    def get_context_data(self, **kwargs):
+        #**kwargs - espera qualquer argumento
+        context = super().get_context_data(**kwargs) #Cria o context
+        context['titulo'] = 'Cadastro de Clientes'
+        context['botao'] = 'Cadastrar'
+        return context
+    
     success_url = reverse_lazy('lista_cliente') #Redireciona para a pagina da tabela apos o cadastro
 
 #Classe para editar os dados
@@ -30,3 +38,11 @@ class Editar(UpdateView):
     form_class = ClienteForm
     template_name = 'clientes/forms_cliente.html'
     success_url = reverse_lazy('lista_cliente')
+    
+    #Deixa dinamico dados no html
+    def get_context_data(self, **kwargs):
+        #**kwargs - espera qualquer argumento
+        context = super().get_context_data(**kwargs) #Cria o context
+        context['titulo'] = 'Edição dos Clientes'
+        context['botao'] = 'Editar'
+        return context
--- Projetos/banco/setup/settings.py
+++ Projetos/banco/setup/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'clientes.apps.ClientesConfig'
+    'clientes.apps.ClientesConfig',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
